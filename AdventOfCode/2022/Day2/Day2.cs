fa1a73b5e6e6c758172fe350395beda34a5247e6
benASTRO <ben.Socials@outlook.de>
2022-12-06 22:27:04 +0100

    Day 2 Part 1

---
+++ AdventOfCode/2022/Day2/Day2.cs
@@ -0,0 +1,75 @@
+﻿
+namespace AdventOfCode._2022.Day2;
+
+public class Day2
+{
+    public int Play()
+    {
+        string input = File.ReadAllText("./2022/Day2/Input.txt");
+        string[] inputSplit = input.Split();
+        int currIndex = 0;
+        int score = 0;
+        int win = 6;
+        int draw = 3;
+        int rock = 1;
+        int paper = 2;
+        int scissors = 3;
+
+        foreach (string item in inputSplit)
+        {
+            if (item is not "Y" and not "X" and not "Z" and not "")
+            {
+                string myChoice = inputSplit[currIndex + 1];
+                score += myChoice switch
+                {
+                    "Y" => paper,
+                    "X" => rock,
+                    "Z" => scissors,
+                    _ => throw new ArgumentOutOfRangeException()
+                };
+
+                switch (item)
+                {
+                    case "A":
+                        if (myChoice is "Y")
+                        {
+                            score += win;
+                        }
+                        else if (myChoice is "X")
+                        {
+                            score += draw;
+                        }
+
+                        break;
+                    case "B":
+                        if (myChoice is "Z")
+                        {
+                            score += win;
+                        }
+                        else if (myChoice is "Y")
+                        {
+                            score += draw;
+                        }
+
+                        break;
+                    case "C":
+                        if (myChoice is "X")
+                        {
+                            score += win;
+                        }
+                        else if (myChoice is "Z")
+                        {
+                            score += draw;
+                        }
+
+                        break;
+                    default: throw new ArgumentOutOfRangeException();
+                }
+            }
+
+            currIndex++;
+        }
+
+        return score;
+    }
+}

80df524f0e986c9e59ed183c72f80058a006fce6
benASTRO <ben.Socials@outlook.de>
2022-12-08 21:43:12 +0100

    Day 2 Part 2

--- AdventOfCode/2022/Day2/Day2.cs
+++ AdventOfCode/2022/Day2/Day2.cs
@@ -3,17 +3,23 @@ namespace AdventOfCode._2022.Day2;
 
 public class Day2
 {
-    public int Play()
+    private static int score;
+
+    private static int currIndex;
+    
+    private const byte win = 6;
+    private const byte draw = 3;
+    
+    private const byte rock = 1;
+    private const byte paper = 2;
+    private const byte scissors = 3;
+
+    public int CalcPart1()
     {
+        currIndex = 0;
+        score = 0;
         string input = File.ReadAllText("./2022/Day2/Input.txt");
         string[] inputSplit = input.Split();
-        int currIndex = 0;
-        int score = 0;
-        int win = 6;
-        int draw = 3;
-        int rock = 1;
-        int paper = 2;
-        int scissors = 3;
 
         foreach (string item in inputSplit)
         {
@@ -72,4 +78,86 @@ public class Day2
 
         return score;
     }
+
+    public int CalcPart2()
+    {
+        currIndex = 0;
+        score = 0;
+        string input = File.ReadAllText("./2022/Day2/Input.txt");
+        string[] inputSplit = input.Split();
+
+        foreach (string item in inputSplit)
+        {
+            if (item is not "Y" and not "X" and not "Z" and not "")
+            {
+                string myChoice = inputSplit[currIndex + 1];
+
+                Options option;
+                option = myChoice switch
+                {
+                    "Y" => Options.Draw,
+                    "X" => Options.Lose,
+                    "Z" => Options.Win,
+                    _ => throw new ArgumentOutOfRangeException()
+                };
+
+                switch (item)
+                {
+                    case "A":
+                        if (option is Options.Win)
+                        {
+                            score += win + paper;
+                        }
+                        else if (option is Options.Draw)
+                        {
+                            score += draw + rock;
+                        }
+                        else
+                        {
+                            score += scissors;
+                        }
+                        break;
+                    case "B":
+                        if (option is Options.Win)
+                        {
+                            score += win + scissors;
+                        }
+                        else if (option is Options.Draw)
+                        {
+                            score += draw + paper;
+                        }
+                        else
+                        {
+                            score += rock;
+                        }
+                        break;
+                    case "C":
+                        if (option is Options.Win)
+                        {
+                            score += win + rock;
+                        }
+                        else if (option is Options.Draw)
+                        {
+                            score += draw + scissors;
+                        }
+                        else
+                        {
+                            score += paper;
+                        }
+                        break;
+                    default: throw new ArgumentOutOfRangeException();
+                }
+            }
+            currIndex++;
+        }
+
+        return score;
+    }
+}
+
+public enum Options
+{
+    Win,
+    Lose,
+    Draw,
 }
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -1,8 +1,15 @@
-﻿using AdventOfCode.Days;
+﻿using System.Threading.Channels;
+using AdventOfCode._2022.Day2;
+using AdventOfCode.Days;
 
 // Day 1
 Day1 day1 = new();
 Console.WriteLine(day1.GetMaxCalories());
 Console.WriteLine(day1.GetSumTopThree());
 
-// Day ...
+Console.WriteLine();
+
+// Day 2
+Day2 day2 = new();
+Console.WriteLine(day2.CalcPart1());
+Console.WriteLine(day2.CalcPart2());
